6df7b5184303b0af9c03d4e4b99b82219d2486f6
Tessie Akin <tolulopeakinwale955@gmail.com>
2025-03-18 14:53:18 +0100

    models updates

--- models/user.model.js
+++ models/user.model.js
@@ -1,12 +1,49 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
 
-//User Schema
 let UserSchema = mongoose.Schema({
-  firstName: {type:String, required:true},
-  lastName: {type:String, required:true},
-  email: {type:String, required:true},
-  Password: {type:String, required:true},
-  registrationDate: {type: Date, default: Date.now} // Added registrationDate field
-})
-let UserModel = mongoose.model('amia-database', UserSchema, 'user_collection');
-module.exports = UserModel
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    age: { type: Number, required: true },
+    password: { type: String, required: true },
+    date: { type: Date, default: Date.now() },
+  });
+
+  // Using Bycrypt to hash user password
+  let saltRound = 10
+  UserSchema.pre('save', function(next){
+    bcrypt.hash(this.password, saltRound, (err, hashedPassword)=>{
+      // console.log(this.password)
+      // console.log(hashedPassword)
+      if(err){
+        console.log("There is an error setting up password", err)
+      }else{
+        this.password = hashedPassword
+        next()
+      }
+    })
+  })
+
+  //Compare hashed password and user inputted password
+  UserSchema.methods.validatePassword = function(password, callback){
+    console.log(this.password, password)
+    bcrypt.compare(password, this.password, (err, same)=>{
+      if(!err){
+        console.log(same)
+        callback(err, same)
+      }else{
+        next()
+      }
+    })
+  }
+
+  
+  
+  let UserModel = mongoose.model(
+    "user_collection",
+    UserSchema,
+    "user_collection"
+  );
+
+  module.exports = UserModel
